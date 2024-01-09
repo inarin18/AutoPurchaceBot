3de53d76f82a9b82656aea8a840d9a34ab9d7cd1
Hinata <69310436+inarin18@users.noreply.github.com>
2024-01-09 22:17:33 +0900

    サブ見出し追加

--- main_app/app.py
+++ main_app/app.py
@@ -6,7 +6,13 @@ from back_end import (
 
 
 def main():
+    
     st.title("自動購入bot")
+    st.write("## ログイン情報") 
+    
+    USER_EMAIL = st.text_input("メールアドレス")
+    
+    
 
 
 if __name__ == "__main__":
